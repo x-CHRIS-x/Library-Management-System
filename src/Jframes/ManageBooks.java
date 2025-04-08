7d194264c800bd5e104e4408144b9bb1585a7fcb
John Chris Ledama  <John Chris Ledama@LAPTOP-NIFERFLT>
2025-04-08 11:56:45 +0800

    clear button

--- src/Jframes/ManageBooks.java
+++ src/Jframes/ManageBooks.java
@@ -306,6 +306,10 @@ public class ManageBooks extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        book_id.setText("");
+        book_name.setText("");
+        author_name.setText("");
+        quantity.setText("");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
